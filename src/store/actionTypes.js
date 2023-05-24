74e5ff5705d7d5a90dd5ae009ffde6851051b942
Vladislav <vlad.argus@gmail.com>
2023-05-24 12:10:37 +0300

    Delete Task

---
+++ src/store/actionTypes.js
@@ -0,0 +1,3 @@
+export const taskUpdated = 'task/updated'
+
+export const taskDeleted = 'task/deleted'
---
+++ src/store/actions.js
@@ -0,0 +1,22 @@
+import * as actionsTypes from './actionTypes'
+
+export function taskCompleted(id) {
+  return {
+    type: actionsTypes.taskUpdated,
+    payload: { id, completed: true },
+  }
+}
+
+export function titleChanged(id) {
+  return {
+    type: actionsTypes.taskUpdated,
+    payload: { id, title: `New title for id${id}` },
+  }
+}
+
+export function taskDeleted(id) {
+  return {
+    type: actionsTypes.taskDeleted,
+    payload: { id },
+  }
+}
